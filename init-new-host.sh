4f93e23d0e4d92dd503ef05ede0534ae17881fb6
Bernhard Halbartschlager <halbart.bernhard+git@gmail.com>
2025-08-05 17:32:11 +0000

    only create new network if not already existing

--- init-new-host.sh
+++ init-new-host.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-docker network create --driver bridge --subnet "10.17.0.0/16" --gateway "10.17.255.254" --ipv6=false caddy
+docker network inspect caddy > /dev/null 2>&1 || \
+    docker network create --driver bridge --subnet "10.17.0.0/16" --gateway "10.17.255.254" --ipv6=false caddy
 
