4aecf508550e36ba791bf8c2299b6c1ca6143fad
Bernhard Halbartschlager <halbart.bernhard+git@gmail.com>
2025-08-05 17:44:16 +0000

    add caddy.env to creator script

--- init-new-host.sh
+++ init-new-host.sh
@@ -3,3 +3,9 @@
 docker network inspect caddy > /dev/null 2>&1 || \
     docker network create --driver bridge --subnet "10.17.0.0/16" --gateway "10.17.255.254" --ipv6=false caddy
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+touch "${SCRIPT_DIR}/caddy.env"
+chown root:root "${SCRIPT_DIR}/caddy.env"
+chmod 0600 "${SCRIPT_DIR}/caddy.env"
+
