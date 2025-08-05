1d3cc18a487a75551c3fbded9d726de780dcdea1
Bernhard Halbartschlager <Bernhard---H@users.noreply.github.com>
2025-08-05 19:08:47 +0200

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM caddy:2-builder AS build
+
+RUN xcaddy build \
+        --with github.com/mholt/caddy-l4/layer4 \
+        --with github.com/mholt/caddy-l4/modules/l4proxy \
+        --with github.com/caddy-dns/cloudflare 
+
+#####################################################################
+FROM caddy:2
+
+RUN apk add bash jq
+
+COPY --from=build /usr/bin/caddy /usr/bin/caddy
+
+ADD ./entrypoint.sh /
+RUN chmod +x /entrypoint.sh
+
+ENTRYPOINT [ "/usr/bin/env", "bash", "/entrypoint.sh" ]
+CMD []
+
---
+++ entrypoint.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# inspired by: https://github.com/mholt/caddy-l4/issues/16#issuecomment-1742044526
+
+set -e
+
+cp -a /etc/caddy/. /conf/
+
+caddy fmt /conf/Caddyfile --overwrite || true
+caddy adapt --config /conf/Caddyfile --pretty --validate > /conf/caddy.json
+
+jsonVars=""
+jqOp=""
+
+
+if [ -d "/conf/apps" ] && [ -n "$(ls -A /conf/apps)" ]; then
+    for fileName in /conf/apps/*.json; do
+
+        appName="$(basename "$fileName" ".json" | tr ' ' '_')"
+        jsonVars="${jsonVars} --slurpfile ${appName} $fileName"
+        if [[ -n "${jqOp}" ]]; then
+            jqOp="${jqOp} |" 
+        fi
+        jqOp="${jqOp} .apps.${appName} = \$${appName}[0]"
+    done
+
+    set -x
+    jq $jsonVars "$jqOp" /conf/caddy.json > /tmp/caddy.json
+    set +x
+    mv /tmp/caddy.json /conf/caddy.json
+    cat /conf/caddy.json
+fi
+
+caddy run --config /conf/caddy.json "$@"
+
