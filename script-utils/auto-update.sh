ecc9d50892afebf933cee8e26de5117abe43573c
Bernhard Halbartschlager <Bernhard---H@users.noreply.github.com>
2025-10-07 01:10:58 +0200

    combine and improve multiple auto-* script features

--- script-utils/auto-update.sh
+++ script-utils/auto-update.sh
@@ -1,15 +1,34 @@
 #!/usr/bin/env bash
 
-# iterate over the container to be upated:
+echo "looking for containers to auto-stop:"
+containerIDs=$(docker container ls --filter "label=at.grub1.auto-stop" --quiet | tr '\n' ' ')
+if [[ -n $containerIDs ]]; then
+    docker container stop $containerIDs
+fi
+
+echo "looking for container to auto-update their image:"
 while IFS= read -r labelsObject; do
     projectDir=$(jq -r '.["com.docker.compose.project.working_dir"]' <<<$labelsObject)
     serviceName=$(jq -r '.["com.docker.compose.service"]' <<<$labelsObject)
-    echo "updating: $serviceName of project: $projectDir"
+    echo "    updating: $serviceName of project: $projectDir"
 
+    # pull new image and deploy:
     docker compose --project-directory "$projectDir" pull "$serviceName"
     docker compose --project-directory "$projectDir" up -d "$serviceName"
 done <<< "$(docker container inspect --format "{{json .Config.Labels}}" $(docker container ls --filter "label=at.grub1.auto-update=image" --quiet))"
 
-echo "done updating, starting cleanup: "
+echo "looking for container to auto-update their built Dockerfile build:"
+while IFS= read -r labelsObject; do
+    projectDir=$(jq -r '.["com.docker.compose.project.working_dir"]' <<<$labelsObject)
+    serviceName=$(jq -r '.["com.docker.compose.service"]' <<<$labelsObject)
+    echo "    updating: $serviceName of project: $projectDir"
+
+    # pull new base image, build new image and deploy:
+    docker compose --project-directory "$projectDir" build --pull "$serviceName"
+    docker compose --project-directory "$projectDir" up -d "$serviceName"
+done <<< "$(docker container inspect --format "{{json .Config.Labels}}" $(docker container ls --filter "label=at.grub1.auto-update=build" --quiet))"
+
+echo "starting image cleanup (at least 24h old): "
 docker image prune -f -a --filter "until=24h"
 
+echo "all done."
