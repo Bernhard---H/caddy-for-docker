ab9f277c8d6f101571ce0208fb8dd45e0bda75df
Bernhard Halbartschlager <Bernhard---H@users.noreply.github.com>
2025-10-26 22:33:41 +0100

    use var

--- script-utils/caddy-ctrl.sh
+++ script-utils/caddy-ctrl.sh
@@ -218,8 +218,9 @@ declare -r RETURN_TRUE=0
 declare -r RETURN_FALSE=1
 
 
-getopt -T
-if [ 4 -ne "$?" ]; then
+getOptTest=0;
+getopt -T || getOptTest="$?";
+if (( getOptTest != 4 )); then
     echo "Please ensure you have the GNU-Version of 'getopt' installed! exiting..." >&2
     exit 1
 fi
