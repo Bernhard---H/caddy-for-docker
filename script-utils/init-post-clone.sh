4189bc0a10672c83749f84645b0d0deab902082a
Bernhard Halbartschlager <Bernhard---H@users.noreply.github.com>
2026-08-28 16:25:35 +0200

    echo i

--- script-utils/init-post-clone.sh
+++ script-utils/init-post-clone.sh
@@ -103,6 +103,7 @@ function extractULA() {
     local ula=""
     IFS=':' read -ra IPV6 <<< "$(extractDockerCidr)"
     for i in "${#IPV6[@]}"; do
+        echo "i: $i"
         if [ $i -lt 1 ]; then
             ula="${IPV6[$i]}"
         elif [ $i -lt 3 ]; then
